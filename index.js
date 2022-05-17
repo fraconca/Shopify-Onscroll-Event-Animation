64332747d2cd4b95fea275e7f987fbdf33186488
fraconca <fraconca@hotmail.com>
2022-05-16 21:24:12 -0300

    Evento Scroll

--- index.js
+++ index.js
@@ -2,12 +2,25 @@
 import './style.css';
 
 // Write Javascript code!
-window.onscroll = function() {myFunction()};
+window.onscroll = function () {
+  minhaFuncao();
+  contaScroll();
+};
 
-function myFunction() {
-  if (document.documentElement.scrollTop > 50) {
-    document.getElementById("myP").className = "test";
-  } else {
-    document.getElementById("myP").className = "";
+function minhaFuncao() {
+  if (document.documentElement.scrollTop < 100) {
+    document.getElementById('caixaNaveia').className = 'caixa45';
+  } else if (document.documentElement.scrollTop > 101) {
+    document.getElementById('caixaNaveia').className = 'caixa0';
+  } 
+  else {
+    document.getElementById('caixaNaveia').className = '';
   }
+}
+
+
+let x = 0;
+function contaScroll() {
+  // document.getElementById("demo").innerHTML = x += 1;
+  // console.log(screenY);
 }
