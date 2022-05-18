0d83cf58a0cb05f7baf3340eb13a60230a796d08
fraconca <fraconca@hotmail.com>
2022-05-17 22:19:22 -0300

    Evolução do Scroll

--- index.js
+++ index.js
@@ -1,41 +1,53 @@
 // Import stylesheets
 import './style.css';
 
-// Write Javascript code!
+// CHAMAR FUNÇÕES ONSCROLL
 window.onscroll = function () {
   minhaFuncao();
   contaScroll();
+  // scrollRotate();
 };
 
+// FUNCAO SELECIONAR ELEMENTOS
 function minhaFuncao() {
   if (document.documentElement.scrollTop < 100) {
-    document.getElementById('caixaNaveia').className = 'animaFlutua bg01';
+    var prop = document.getElementById('caixaNaveia');
+    prop.className = 'animaFlutua bg01';
+    scrollRotate(-45);
   } else if (document.documentElement.scrollTop < 200) {
     var prop = document.getElementById('caixaNaveia');
     prop.className = 'animaFlutua bg01';
-  } else if (document.documentElement.scrollTop < 300) {
-    document.getElementById('caixaNaveia').className = 'animaFlutua bg02';
-  } else if (document.documentElement.scrollTop < 400) {
-    document.getElementById('caixaNaveia').className = 'animaFlutua bg03';
+    scrollRotate(-90);
+  } else if (document.documentElement.scrollTop < 500) {
+    var prop = document.getElementById('caixaNaveia');
+    prop.className = 'animaFlutua bg02';
+    scrollRotate(-120);
+  } else if (document.documentElement.scrollTop < 900) {
+    var prop = document.getElementById('caixaNaveia');
+    prop.className = 'animaFlutua bg03';
+    scrollRotate(-150);
   } else {
-    document.getElementById('caixaNaveia').className = 'animaFlutua bg04';
+    var prop = document.getElementById('caixaNaveia');
+    prop.className = 'animaFlutua bg04';
   }
 }
 
-window.onscroll = function () {
-  scrollRotate();
-};
-
-function scrollRotate() {
+// FUNCAO ROTACIONAR
+function scrollRotate(valRotaciona) {
+  let valor = valRotaciona;
+  console.log('ROTACIONAR: '+ valor);
+  let rotacionar = valor + window.pageYOffset / 40;
   let image = document.getElementById('caixaNaveia');
-  image.style.transform = 'rotate(' + window.pageYOffset / 4 + 'deg)';
+  image.style.transform = 'translateY(45px) rotate(' + rotacionar + 'deg)';
+  // image.style.animation ('flutuando 5s ease-in-out infinite');
+
+  
 }
 
+// FUNCAO CALCULA SCROLL
 function contaScroll() {
-  let altura = document.documentElement.scrollTop;
+  var altura = document.documentElement.scrollTop;
   console.log(altura);
-  // document.getElementById('caixaNaveia').style.transform = rotate(180);
-  // console.log((altura += 100));
 }
 
 // // var caixaA = document.getElementById("roda1"), caixaB = document.getElementById("roda2");
