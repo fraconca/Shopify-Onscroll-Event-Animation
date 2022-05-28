8cd97d252b20dfe3c184d62dad214b61493088ac
fraconca <fraconca@hotmail.com>
2022-05-28 20:19:42 -0300

    2 files modified

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ function minhaFuncao() {
     var prop = document.getElementById('caixaNaveia');
     prop.className = 'bg02';
     scrollRotate(-30);
-  } else if (document.documentElement.scrollTop < 1950) {
+  } else if (document.documentElement.scrollTop < 1750) {
     var prop = document.getElementById('caixaNaveia');
     prop.className = 'bg03';
     scrollRotate(-30);
